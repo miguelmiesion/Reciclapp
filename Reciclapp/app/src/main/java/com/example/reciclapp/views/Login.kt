3658ee599c310924af1526ff7a914f6f0d96b548
lucagior22 <lucagior22@gmail.com>
2025-12-11 17:54:02 -0300

    Login más sólido

--- Reciclapp/app/src/main/java/com/example/reciclapp/views/Login.kt
+++ Reciclapp/app/src/main/java/com/example/reciclapp/views/Login.kt
@@ -1,7 +1,9 @@
 package com.example.reciclapp.views
 
+import android.app.AlertDialog
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -39,7 +41,10 @@ import java.net.HttpURLConnection
 import java.net.URI
 import java.net.URL
 import androidx.compose.ui.platform.LocalContext
+import com.example.reciclapp.components.LocalPopupState
 import com.example.reciclapp.network.TokenManager
+import com.example.reciclapp.ui.theme.DarkerPrimary
+import com.example.reciclapp.ui.theme.LightTextColor
 
 @Composable
 fun LoginScreen(navController: NavController) {
@@ -50,212 +55,212 @@ fun LoginScreen(navController: NavController) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var passwordVisible by remember { mutableStateOf(false) }
-    var rememberMe by remember { mutableStateOf(false) }
 
     var statusMessage by remember { mutableStateOf("") }
-    var statusColor by remember { mutableStateOf(Color.Gray) }
+    var isLoading by remember { mutableStateOf(false) }
+
+    var popupController = LocalPopupState.current
 
     val apiUrl = BuildConfig.API_URL
-    Column(
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp)
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .imePadding(),
+        contentAlignment = Alignment.TopCenter
     ) {
-        // 1. Header Logo
-        Text(
-            text = "Reciclapp",
-            color = ReciclappGreen,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            modifier = Modifier.align(Alignment.Start)
-        )
 
-        Spacer(modifier = Modifier.height(40.dp))
-
-        // 2. Title
-        Text(
-            text = "Login",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(Alignment.Start)
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        val annotatedString = buildAnnotatedString {
-            append("Si no tenés una cuenta registrada podés ")
-
-            val link = LinkAnnotation.Clickable(
-                tag = "REGISTER_LINK",
-                linkInteractionListener = {
-                    navController.navigate("register_screen") {
-                        popUpTo("login_screen") { inclusive = true }
-                    }
-                }
-            )
-            withLink(link) {
-                withStyle(SpanStyle(color = ReciclappGreen, fontWeight = FontWeight.Bold)) {
-                    append("registrarte acá !")
-                }
-            }
-        }
-
-        Text(
-            text = annotatedString,
-            fontSize = 14.sp,
-            color = Color.Gray,
-            modifier = Modifier.align(Alignment.Start)
-        )
-
-        Spacer(modifier = Modifier.height(30.dp))
-
-        TextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Nombre de usuario") },
-            placeholder = { Text("Ingresá tu nombre de usuario") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "User Icon") },
-            modifier = Modifier.fillMaxWidth(),
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-                disabledContainerColor = Color.Transparent,
-                focusedIndicatorColor = Color.Black,
-                unfocusedIndicatorColor = Color.Gray
-            ),
-            singleLine = true
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Contraseña") },
-            placeholder = { Text("Ingresá tu contraseña") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock Icon") },
-            trailingIcon = {
-                val image = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                    Icon(imageVector = image, contentDescription = "Toggle Password")
-                }
-            },
-            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            modifier = Modifier.fillMaxWidth(),
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-                disabledContainerColor = Color.Transparent,
-                focusedIndicatorColor = Color.Black,
-                unfocusedIndicatorColor = Color.Gray
-            ),
-            singleLine = true
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    checked = rememberMe,
-                    onCheckedChange = { rememberMe = it },
-                    colors = CheckboxDefaults.colors(checkedColor = ReciclappGreen)
-                )
-                Text(text = "Recuérdame", fontSize = 14.sp, color = Color.DarkGray)
-            }
-
-            Text(
-                text = "Olvidaste tu contraseña?",
-                fontSize = 12.sp,
-                color = Color.Gray,
-                modifier = Modifier.clickable { /* Handle Forgot Password */ }
-            )
-        }
-
-        Spacer(modifier = Modifier.height(30.dp))
-
-        Button(
-            onClick = {
-                statusMessage = "Iniciando sesión..."
-                statusColor = Color.Gray
-
-                CoroutineScope(Dispatchers.IO).launch {
-                    try {
-                        val url: URL = URI.create(apiUrl + "api/login/").toURL()
-                        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-
-                        connection.requestMethod = "POST"
-                        connection.setRequestProperty("Content-Type", "application/json; utf-8")
-                        connection.setRequestProperty("Accept", "application/json")
-                        connection.doOutput = true
-
-                        val jsonObject = JSONObject()
-                        jsonObject.put("username", username)
-                        jsonObject.put("password", password)
-
-                        connection.outputStream.use { os ->
-                            val input = jsonObject.toString().toByteArray(Charsets.UTF_8)
-                            os.write(input, 0, input.size)
-                        }
-
-                        val responseCode = connection.responseCode
-
-                        if (responseCode == HttpURLConnection.HTTP_OK) {
-                            val response = connection.inputStream.bufferedReader().use { it.readText() }
-                            val jsonResponse = JSONObject(response)
-
-                            val accessToken = jsonResponse.getString("access")
-                            val refreshToken = jsonResponse.getString("refresh")
-
-                            tokenManager.saveTokens(accessToken, refreshToken)
-
-                            CoroutineScope(Dispatchers.Main).launch {
-                                statusMessage = "Login Exitoso!"
-                                statusColor = ReciclappGreen
-                                navController.navigate("home_screen") {
-                                    popUpTo("login_screen") { inclusive = true }
-                                }
-                            }
-                        } else {
-                            statusMessage = "Error: Usuario o contraseña incorrectos"
-                            statusColor = Color.Red
-                        }
-                        connection.disconnect()
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                        CoroutineScope(Dispatchers.Main).launch {
-                            statusMessage = "Error de conexión"
-                            statusColor = Color.Red
-                        }
-                    }
-                }
-            },
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(50.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = ReciclappGreen),
-            shape = RoundedCornerShape(25.dp) // More rounded as per image
+                .padding(start = 24.dp, end = 24.dp, top = 24.dp, bottom = 0.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Login", fontSize = 18.sp, fontWeight = FontWeight.Bold)
-        }
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        // Status Message for Errors
-        if (statusMessage.isNotEmpty()) {
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(12.dp))
+            // 1. Header Logo
             Text(
-                text = statusMessage,
-                color = statusColor,
-                fontWeight = FontWeight.Bold
+                text = "Reciclapp",
+                color = DarkerPrimary,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                modifier = Modifier.align(Alignment.Start)
             )
+
+            Spacer(modifier = Modifier.height(72.dp))
+
+            // 2. Title
+            Text(
+                text = "Login",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Start)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            val annotatedString = buildAnnotatedString {
+                append("Si no tenés una cuenta registrada podés ")
+
+                val link = LinkAnnotation.Clickable(
+                    tag = "REGISTER_LINK",
+                    linkInteractionListener = {
+                        navController.navigate("register_screen") {
+                            popUpTo("login_screen") { inclusive = true }
+                        }
+                    }
+                )
+                withLink(link) {
+                    withStyle(SpanStyle(color = DarkerPrimary, fontWeight = FontWeight.Bold)) {
+                        append("registrarte acá !")
+                    }
+                }
+            }
+
+            Text(
+                text = annotatedString,
+                fontSize = 14.sp,
+                color = Color.Gray,
+                modifier = Modifier.align(Alignment.Start)
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Nombre de usuario") },
+                placeholder = { Text("Ingresá tu nombre de usuario") },
+                leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "User Icon") },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Black,
+                    unfocusedIndicatorColor = Color.Gray
+                ),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Contraseña") },
+                placeholder = { Text("Ingresá tu contraseña") },
+                leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock Icon") },
+                trailingIcon = {
+                    val image = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(imageVector = image, contentDescription = "Toggle Password")
+                    }
+                },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Black,
+                    unfocusedIndicatorColor = Color.Gray
+                ),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Olvidaste tu contraseña?",
+                    fontSize = 12.sp,
+                    color = Color.Gray,
+                    modifier = Modifier.clickable {
+                        AlertDialog.Builder(context)
+                            .setMessage("TO-DO!")
+                            .setTitle("Funcionalidad no desarrollada :(!")
+                            .create()
+                            .show()
+                    }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+                onClick = {
+                    statusMessage = "Iniciando sesión..."
+                    isLoading = true
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        try {
+                            val url: URL = URI.create(apiUrl + "api/login/").toURL()
+                            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+                            connection.requestMethod = "POST"
+                            connection.setRequestProperty("Content-Type", "application/json; utf-8")
+                            connection.setRequestProperty("Accept", "application/json")
+                            connection.doOutput = true
+
+                            val jsonObject = JSONObject()
+                            jsonObject.put("username", username)
+                            jsonObject.put("password", password)
+
+                            connection.outputStream.use { os ->
+                                val input = jsonObject.toString().toByteArray(Charsets.UTF_8)
+                                os.write(input, 0, input.size)
+                            }
+
+                            val responseCode = connection.responseCode
+
+                            if (responseCode == HttpURLConnection.HTTP_OK) {
+                                val response = connection.inputStream.bufferedReader().use { it.readText() }
+                                val jsonResponse = JSONObject(response)
+
+                                val accessToken = jsonResponse.getString("access")
+                                val refreshToken = jsonResponse.getString("refresh")
+
+                                tokenManager.saveTokens(accessToken, refreshToken)
+
+                                CoroutineScope(Dispatchers.Main).launch {
+                                    navController.navigate("home_screen") {
+                                        popUpTo("login_screen") { inclusive = true }
+                                    }
+                                }
+                            } else {
+                                statusMessage = "Error: Usuario o contraseña incorrectos"
+                            }
+                            connection.disconnect()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            CoroutineScope(Dispatchers.Main).launch {
+                                statusMessage = "Error de conexión"
+                            }
+                        } finally {
+                            isLoading = false
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = DarkerPrimary),
+                shape = RoundedCornerShape(25.dp) // More rounded as per image
+            ) {
+                Text(text = if (!isLoading) "Login" else statusMessage, fontSize = 18.sp, fontWeight = FontWeight.Bold, color = LightTextColor)
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            if (statusMessage.isNotEmpty() && !isLoading) {
+                popupController.showError(statusMessage)
+            }
         }
     }
 }

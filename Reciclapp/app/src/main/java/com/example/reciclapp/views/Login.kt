b8b1f48f0ecba5cbf498268aae2b5355172de1e8
lucagior22 <lucagior22@gmail.com>
2025-12-11 22:47:20 -0300

    un ícono

--- Reciclapp/app/src/main/java/com/example/reciclapp/views/Login.kt
+++ Reciclapp/app/src/main/java/com/example/reciclapp/views/Login.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
@@ -129,7 +130,7 @@ fun LoginScreen(navController: NavController) {
                 onValueChange = { username = it },
                 label = { Text("Nombre de usuario") },
                 placeholder = { Text("Ingresá tu nombre de usuario") },
-                leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "User Icon") },
+                leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "User Icon") },
                 modifier = Modifier.fillMaxWidth(),
                 colors = TextFieldDefaults.colors(
                     focusedContainerColor = Color.Transparent,
